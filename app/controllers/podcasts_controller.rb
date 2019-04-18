7a6b23be55f5cf229d968002656d905b8d7a27f5
Gwenny Warnick <gwendolen.warnick@gmail.com>
2019-04-18 12:08:12 +1000

    added episodes controller

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -6,4 +6,26 @@ class PodcastsController < ApplicationController
   def show
     @podcast = Podcast.find(params[:id])
   end
+
+  def new
+    @podcast = Podcast.new
+  end
+
+  def create
+    @podcast = Podcast.new(podcast_params)
+
+    if @podcast.save
+      redirect_to @podcast
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    
+  end
+
+  def delete
+  end
+
 end
